9793a01b349295045ac6d7261885653c99fc994d
Bekzhanym <maukenovbekzan@gmail.com>
2026-05-01 16:30:00 +0500

    bootstrap: ServiceFactory

---
+++ src/main/java/edu/university/bootstrap/ServiceFactory.java
@@ -0,0 +1,37 @@
+package edu.university.bootstrap;
+
+import edu.university.application.factory.UserFactory;
+import edu.university.application.usecase.AdvancedSearchUseCase;
+import edu.university.application.usecase.RegisterStudentForCourseUseCase;
+import edu.university.application.usecase.ResearcherDirectoryUseCase;
+import edu.university.infrastructure.persistence.UniversityDatabase;
+
+/**
+ * Composition root: wires infrastructure adapters to application use cases.
+ */
+public final class ServiceFactory {
+    private static final UniversityDatabase DATABASE = UniversityDatabase.getInstance();
+
+    private ServiceFactory() {
+    }
+
+    public static UniversityDatabase database() {
+        return DATABASE;
+    }
+
+    public static UserFactory userFactory() {
+        return new UserFactory();
+    }
+
+    public static AdvancedSearchUseCase advancedSearch() {
+        return new AdvancedSearchUseCase(DATABASE, DATABASE);
+    }
+
+    public static ResearcherDirectoryUseCase researcherDirectory() {
+        return new ResearcherDirectoryUseCase(DATABASE);
+    }
+
+    public static RegisterStudentForCourseUseCase registerStudentForCourse() {
+        return new RegisterStudentForCourseUseCase();
+    }
+}
