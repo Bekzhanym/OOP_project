bad22ebf44986aab81e2c72bd102dc16295a7952
alizhantulep <tulepov06@icloud.com>
2026-05-17 13:08:47 +0500

    Update OOP project

--- src/kbtu_oop_project/domain/value/Role.java
+++ src/kbtu_oop_project/domain/value/Role.java
@@ -2,7 +2,6 @@ package kbtu_oop_project.domain.value;
 
 public enum Role {
     STUDENT,
-    /** Бакалавр 4-го курса с возможным научным руководителем (`Student4thYear`). */
     STUDENT_4TH_YEAR,
     TEACHER,
     EMPLOYEE,
